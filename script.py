1b0d9ef34974c324551878a73db3aa1479d6173e
Advaithva <97515865+Advaithva@users.noreply.github.com>
2022-04-12 18:27:55 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,10 @@
+# Area calculator of square and Rectangle
+# Run the code 
+# Enter the Length and Breath and you can find the Area
+# Note - Do not edit the code
+length = input('Length')
+breadth = input('Breadth')
+Area = int(length) * int(breadth)
+print (Area)
+Perimeter = int(2) * int(length) + int (2) * int(breadth)
+print(Perimeter)
